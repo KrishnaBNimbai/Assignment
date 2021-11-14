7eb744ab29570a3c3e148ff0bde7c1e1931c9b1c
KrishnaBNimbai <73700230+KrishnaBNimbai@users.noreply.github.com>
2021-11-14 19:34:08 +0530

    styling
    changed  font style

---
+++ nov 12/14/10/21/11/11/21/wishes.js
@@ -0,0 +1,18 @@
+function gm(){
+    var h1ref = document.getElementById("h1");
+    h1ref.innerText="Good Morning ";
+    h1ref.style.color="red";
+
+        }
+function ge(){
+    var h1ref = document.getElementById("h1");
+    h1ref.innerText="Good Evening ";
+    h1ref.style.color="green";        
+
+        }
+function gn(){
+    var h1ref = document.getElementById("h1");
+    h1ref.innerText="Good Night ";
+    h1ref.style.color="blue";
+
+        } 
